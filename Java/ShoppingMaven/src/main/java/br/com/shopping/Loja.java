4c7faa54dcb5f5d491fd4dbd610ea90c6d6fc425
Marco87 <marco.conviccao@gmail.com>
2019-08-03 10:33:50 -0300

    Atualização do repositório do Git

---
+++ Java/ShoppingMaven/src/main/java/br/com/shopping/Loja.java
@@ -0,0 +1,17 @@
+package br.com.shopping;
+import java.time.LocalDate;
+
+public class Loja {
+	private int idLoja;
+	private String nomeLoja;
+	private String cnpjLoja;
+	private String segmentoLoja;
+	private String pisoLoja;
+	private String numeroLoja;
+	private LocalDate dataSaidaLoja;
+
+	
+	public static void main (String args[]) {
+		System.out.println("Hello World!");
+	}
+}
